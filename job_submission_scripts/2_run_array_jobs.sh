1eb634b51f34b5068118bd6135890a786d9096e9
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2025-08-25 09:28:11 -0600

    fix(typo): job submission script now points to correct path, script 2

--- job_submission_scripts/2_run_array_jobs.sh
+++ job_submission_scripts/2_run_array_jobs.sh
@@ -13,4 +13,4 @@ subject_list=($(cat /project/ctb-rmcintos/jwangbay/DO_NOT_USE_THIS_OR_jwangbay_O
 SUB="${subject_list[$((SLURM_ARRAY_TASK_ID-1))]}"
 
 # Run Python script for this subject
-python ~/scratch/BRS_Training/2_process_one_subject.py "$SUB"
+python ~/scratch/BRS_Training/job_submission_scripts/2_process_one_subject.py "$SUB"
